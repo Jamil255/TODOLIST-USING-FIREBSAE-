e99ad28a28fb30ceb4d9bf94d302e8f5aaa19552
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2025-03-24 08:16:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
